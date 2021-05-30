4a9694236bf9b149f7f86ce49ba0150d07394868
Infarh <shmachilin@yandex.ru>
2021-05-30 18:25:48 +0300

    Рефайторинг LocationInfo

--- MetaWeather/Models/LocationInfo.cs
+++ MetaWeather/Models/LocationInfo.cs
@@ -22,75 +22,72 @@ namespace MetaWeather.Models
 
         [JsonPropertyName("sources")]
         public Source[] Sources { get; set; }
+        public class Source
+        {
+            [JsonPropertyName("title")]
+            public string Title { get; set; }
+
+            [JsonPropertyName("slug")]
+            public string Slug { get; set; }
+
+            [JsonPropertyName("url")]
+            public string Url { get; set; }
+
+            [JsonPropertyName("crawl_rate")]
+            public int CrawlRate { get; set; }
+        }
 
         [JsonPropertyName("timezone")]
         public string Timezone { get; set; }
 
         [JsonPropertyName("consolidated_weather")]
         public LocationWeatherInfo[] Weather { get; set; }
-    }
+        public class LocationWeatherInfo
+        {
+            [JsonPropertyName("id")]
+            public long Id { get; set; }
 
-    public class LocationWeatherInfo
-    {
-        [JsonPropertyName("id")] 
-        public long Id { get; set; }
-        
-        [JsonPropertyName("weather_state_name")]
-        public string WeatherStateName { get; set; }
-        
-        [JsonPropertyName("weather_state_abbr")]
-        public string WeatherStateAbbr { get; set; }
-        
-        [JsonPropertyName("wind_direction_compass")]
-        public string WindDirectionCompass { get; set; }
-        
-        [JsonPropertyName("created")]
-        public DateTime Created { get; set; }
-        
-        [JsonPropertyName("applicable_date")]
-        public DateTime ApplicableDate { get; set; }
-        
-        [JsonPropertyName("min_temp")]
-        public double MinTemp { get; set; }
-        
-        [JsonPropertyName("max_temp")]
-        public double MaxTemp { get; set; }
-        
-        [JsonPropertyName("the_temp")]
-        public double TheTemp { get; set; }
-        
-        [JsonPropertyName("wind_speed")]
-        public double WindSpeed { get; set; }
-        
-        [JsonPropertyName("wind_direction")]
-        public double WindDirection { get; set; }
-        
-        [JsonPropertyName("air_pressure")]
-        public double AirPressure { get; set; }
-        
-        [JsonPropertyName("humidity")]
-        public int Humidity { get; set; }
-        
-        [JsonPropertyName("visibility")]
-        public double Visibility { get; set; }
-        
-        [JsonPropertyName("predictability")]
-        public int Predictability { get; set; }
-    }
+            [JsonPropertyName("weather_state_name")]
+            public string WeatherStateName { get; set; }
 
-    public class Source
-    {
-        [JsonPropertyName("title")] 
-        public string Title { get; set; }
-        
-        [JsonPropertyName("slug")]
-        public string Slug { get; set; }
-        
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-        
-        [JsonPropertyName("crawl_rate")]
-        public int CrawlRate { get; set; }
-    }
+            [JsonPropertyName("weather_state_abbr")]
+            public string WeatherStateAbbr { get; set; }
 
+            [JsonPropertyName("wind_direction_compass")]
+            public string WindDirectionCompass { get; set; }
+
+            [JsonPropertyName("created")]
+            public DateTime Created { get; set; }
+
+            [JsonPropertyName("applicable_date")]
+            public DateTime ApplicableDate { get; set; }
+
+            [JsonPropertyName("min_temp")]
+            public double MinTemp { get; set; }
+
+            [JsonPropertyName("max_temp")]
+            public double MaxTemp { get; set; }
+
+            [JsonPropertyName("the_temp")]
+            public double TheTemp { get; set; }
+
+            [JsonPropertyName("wind_speed")]
+            public double WindSpeed { get; set; }
+
+            [JsonPropertyName("wind_direction")]
+            public double WindDirection { get; set; }
+
+            [JsonPropertyName("air_pressure")]
+            public double AirPressure { get; set; }
+
+            [JsonPropertyName("humidity")]
+            public int Humidity { get; set; }
+
+            [JsonPropertyName("visibility")]
+            public double Visibility { get; set; }
+
+            [JsonPropertyName("predictability")]
+            public int Predictability { get; set; }
+        }
+    }
 }
